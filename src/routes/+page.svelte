295a6cf2f6d3081ef7b0c7c1950195421c60cced
tstock <thomas.stock@aprimo.com>
2023-03-28 10:44:42 +0200

    extract calculateCanvas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import calculateCanvas from './calculateCanvas';
 
 	let canvas: HTMLCanvasElement;
 	let box: HTMLDivElement;
 
-	// The canvasScale represents how much % bigger the <canvas> is in px compared to the map
+	// The scale represents how much % bigger the <canvas> is in px compared to the map
 	// (They have the same aspect ratio)
-	let canvasScale: number;
+	let scale: number;
 
 	onMount(() => {
 		const resizeObserver = new ResizeObserver((entries) => {
@@ -14,26 +15,11 @@
 			const boxRect = entry.contentRect;
 
 			const map = { width: 400, height: 800 };
-			const mapAspectRatio = map.width / map.height;
 
-			// Aspect ratio of the viewport (box fills viewport via css)
-			const boxAspectRatio = boxRect.width / boxRect.height;
-
-			// Resize the canvas so that it has the same aspect ratio as our map.
-			// Canvas is centered with css so there is some 'leftover' either left/right or bottom.top
-			//  while we make it as big as possible.
-			if (mapAspectRatio > boxAspectRatio) {
-				canvas.width = boxRect.width;
-				canvas.height = boxRect.width / mapAspectRatio;
-			} else {
-				canvas.height = boxRect.height;
-				canvas.width = boxRect.height * mapAspectRatio;
-			}
-
-			// Now we know the <canvas> is for example 2000*1000 and our map could be 100*50 so scale is '20'.
-			// Which means when we draw a dot on the center of the map at (50,25),
-			//  we should actually draw it at x20 these coordinates so at (1000,500) to draw in the center of the <canvas>
-			canvasScale = canvas.width / map.width;
+			const { width, height, scale: newScale } = calculateCanvas({ map, boxRect });
+			canvas.width = width;
+			canvas.height = height;
+			scale = newScale;
 		});
 		resizeObserver.observe(box);
 
@@ -42,6 +28,6 @@
 </script>
 
 <div class="w-screen h-screen bg-yellow-300" bind:this={box}>
-	<div>Scale: {canvasScale}</div>
+	<div>Scale: {scale}</div>
 	<canvas class="m-auto absolute inset-0 bg-blue-600" bind:this={canvas} />
 </div>
